5f8dd2c124ea95df458c800721f14e2a37728ade
kevin-vargas <kevinvargasnavia@gmail.com>
2022-02-26 22:05:03 -0300

    make it process

--- main.go
+++ main.go
@@ -3,16 +3,31 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 const POD_ID = "HOSTNAME"
 
 func main() {
-	podId := os.Getenv(POD_ID)
-	if len(podId) > 0 {
-		fmt.Println("POD ID")
-		fmt.Println(podId)
-	} else {
-		fmt.Println("NO HOSTNAME ENV")
+	fmt.Println("Running Sidecar logger")
+	ticker := time.NewTicker(60 * time.Second)
+	quit := make(chan struct{})
+	for {
+		select {
+		case <-ticker.C:
+			go func() {
+				podId := os.Getenv(POD_ID)
+				if len(podId) > 0 {
+					fmt.Println("POD ID")
+					fmt.Println(podId)
+				} else {
+					fmt.Println("NO HOSTNAME ENV")
+				}
+			}()
+		case <-quit:
+			ticker.Stop()
+			return
+		}
 	}
+
 }
